e20ee219fd53dcd25e329ae904aa86b2cd1aa08c
BT_1N3_01 <sjiang9453@bths.edu>
2018-10-01 10:12:01 -0400

    ETriangle Done

--- shapelab/src/shape.java
+++ shapelab/src/shape.java
@@ -14,4 +14,22 @@ public abstract class shape {
     {
         return "I am a " + type + ".";
     }
+
+    public class ETriangle extends shape
+    {
+        private int side;
+        public ETriangle(int side)
+        {
+            super(type:"ETriangle");
+            this.side = side;
+        }
+        public double getArea()
+        {
+            return 0.433*(side*side);
+        }
+        public double getPerimeter();
+        {
+            return side*3;
+        }
+    }
 }
